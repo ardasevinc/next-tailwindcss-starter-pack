299c131a99e398fa1962d3b7f2f3d820426b2889
Arda Sevinç <ardasevinc.technical@gmail.com>
2022-09-07 23:38:50 +0300

    lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
       <Head>
         <title>tailwind starter template</title>
       </Head>
-      <main className="bg-slate-900 text-white flex-1 flex flex-col items-center justify-center gap-y-4">
+      <main className="flex flex-1 flex-col items-center justify-center gap-y-4 bg-slate-900 text-white">
         <h1 className="text-4xl font-bold">Hello World!</h1>
         <p>I can&apos;t wait to see what you create!</p>
       </main>
